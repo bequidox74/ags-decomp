0774cb5b76689e8968bc0564d04f085a84028a51
bequidox74 <aw4yland04@proton.me>
2026-09-05 22:52:25 +0300

    improve text output

--- disassembler.py
+++ disassembler.py
@@ -142,23 +142,6 @@ class Instruction(NamedTuple):
     opcode: Opcode
     params: list[Parameter]
 
-    def format(self) -> str:
-        p: list = self.params.copy()
-        opc = self.opcode
-        assert opc.nargs == len(p), "Instruction parameter count mismatch"
-        for i in range(len(p)):
-            match opc.args_format[i]:
-                case "r":
-                    p[i] = Register(p[i]).name
-                case "a":
-                    pass  # leave as is (TODO: float heuristic?)
-                case _:
-                    raise RuntimeError(f"Illegal args format: {opc.args_format}")
-        return f"{self.opcode.mnemonic} {sjoin(", ", p)}"
-
-    def __str__(self) -> str:
-        return self.format()
-
 
 class Function(NamedTuple):
     name: str
@@ -167,19 +150,29 @@ class Function(NamedTuple):
     instructions: list[Instruction]
 
 
-@dataclass
 class Disassembly:
-    functions: list[Function]
-    strings: list[str]
-    gdata: bytes
+    def __init__(self, source) -> None:
+        self.source = source
+        self.functions: list[Function] = []
+        self.gdata: bytes = b""
+        self.scomVersion: int = 90
 
-    def format(self, sw: StringWriter | None = None, **kwargs) -> str:
+        self.code: list[int] = []
+        self._read_scom()
+
+        self._disassemble()
+
+    def format(self, sw: StringWriter | None = None) -> str:
         if sw is None:
             sw = StringWriter()
 
+        sw.println(f"; AGS SCOM version {self.scomVersion}")
+
+        # printing bytes isn't very elegant, but it's the only way to be sure
+        # it's correct, at least until a proper analyzer is implemented.
         if self.gdata:
             sw.cr()
-            sw.println(".data")
+            sw.println(f".data[{len(self.gdata)}]")
             sw.indent()
             for line in format_bindata(self.gdata):
                 sw.println(line)
@@ -188,82 +181,108 @@ class Disassembly:
 
         if self.functions:
             sw.cr()
-            sw.println(".code")
+            sw.println(f".code[{self.code_size}]")
             for func in self.functions:
-                sw.println(f"{func.name}${func.nargs}: ; @0x{func.offset:X}")
+                sw.println(f"{func.name}${func.nargs}: ; @{func.offset}")
+                sw.indent()
                 sw.indent()
                 for ins in func.instructions:
+                    is_linenum = ins.opcode == Opcode.LINENUM
                     out = ins.opcode.mnemonic
+                    if is_linenum:
+                        sw.dedent()
                     if ins.params:
                         out += f" {sjoin(", ", ins.params)}"
                     sw.println(out)
+                    if is_linenum:
+                        sw.indent()
                 sw.dedent()
                 sw.println()
 
         if self.strings:
             sw.cr()
-            sw.println(".strings")
-            width = len(str(len(self.strings)))
-            for i, s in enumerate(self.strings):
-                idx = format(i, f"{width}")
-                sw.println(f"{idx}:{quote(s)}")
+            sw.println(f".strings[{self.strings_size}] ; {len(self.strings)} strings")
+            for i, s in self._indexed_strings.items():
+                sw.println(f"{i}: {quote(s)}")
+            sw.println()
+
+        if self.imports:
+            sw.cr()
+            sw.println(
+                f".imports[{self.imports_size}] ; {self.imports_size} names, {len(self.imports)} referenced"
+            )
+            for offset, name in self.imports.items():
+                sw.println(f"{offset}: {name}")
+            sw.println()
+
+        if self.exports:
+            sw.cr()
+            sw.println(f".exports[{self.exports_size}]")
+            for offset, export in self.exports.items():
+                comment = " ; function" if "$" in export.name else " ; variable"
+                sw.println(f"{offset}: {export.name}{comment}")
+            sw.println()
+
+        if self.fixups:
+            sw.cr()
+            sw.println(f".fixups[{len(self.fixups)}]")
+            for idx, type_ in self.fixups.items():
+                sw.println(f"{idx}: {type_.name.lower()}")
+            sw.println()
+
+        if self.sections:
+            sw.cr()
+            sw.println(f".sections[{self.sections_size}]")
+            for s in self.sections:
+                sw.println(f"{s.offset}: {s.name}")
 
         return str(sw)
 
     def __str__(self) -> str:
         return self.format()
 
-
-class Disassembler:
-    def disassemble(self, source) -> Disassembly:
-        self.source = source
-        self.ds = Disassembly([], [], b"")
-        self._read_scom()
-        self._disassemble()
-        return self.ds
-
     def _read_scom(self) -> None:
         br = BinaryReader(self.source)
 
         assert br.read_fixed_string(4) == "SCOM", "Source is not a compiled AGS script"
-        assert br.u32() == 90, "Unsupported SCOM version"
+        self.scomVersion = br.u32()
+        assert self.scomVersion == 90, "Unsupported SCOM version"
 
         self.gdata_size = br.u32()
         self.code_size = br.u32()
         self.strings_size = br.u32()
 
         if self.gdata_size > 0:
-            self.ds.gdata = br.read_bytes(self.gdata_size)
+            self.gdata = br.read_bytes(self.gdata_size)
 
-        self.code: list[int] = []
         for _ in range(self.code_size):
             self.code.append(br.u32())
 
-        self.strings: dict[int, str] = {}
+        self._indexed_strings: dict[int, str] = {}
         strings_start = br.tell()
         while br.tell() < strings_start + self.strings_size:
             # remember string offsets for later fixups
             offset = br.tell() - strings_start
-            self.strings[offset] = br.cstr()
-        self.ds.strings = list(self.strings.values())
+            self._indexed_strings[offset] = br.cstr()
+        self.strings = list(self._indexed_strings.values())
 
-        self.fixups_size = br.u32()
+        fixups_size = br.u32()
         self.fixups: dict[int, FixupType] = {}
         types = []
         offsets = []
-        for _ in range(self.fixups_size):
+        for _ in range(fixups_size):
             types.append(br.u8())
-        for _ in range(self.fixups_size):
+        for _ in range(fixups_size):
             offsets.append(br.u32())
-        for i in range(self.fixups_size):
+        for i in range(fixups_size):
             self.fixups[offsets[i]] = FixupType(types[i])
 
         self.imports_size = br.u32()
         self.imports: dict[int, str] = {}
-        for _ in range(self.imports_size):
+        for i in range(self.imports_size):
             import_ = br.cstr()
             if import_:  # skip empty imports
-                self.imports[br.tell()] = import_
+                self.imports[i] = import_
 
         self.exports_size = br.u32()
         self.exports: dict[int, Export] = {}
@@ -299,7 +318,7 @@ class Disassembler:
 
         for s, e in ranges:
             func = self._dis_function(s, e)
-            self.ds.functions.append(func)
+            self.functions.append(func)
 
     def _dis_function(self, start: int, end: int) -> Function:
         mangled_name = self.func_names[start]
@@ -330,11 +349,14 @@ class Disassembler:
                     if idx in self.fixups:
                         match self.fixups[idx]:
                             case FixupType.STRING:
-                                params.append(quote(self.strings[arg]))
+                                params.append(quote(self._indexed_strings[arg]))
                             case FixupType.GLOBAL_DATA:
                                 params.append(f"@{arg}")
+                            case FixupType.IMPORT:
+                                params.append(self.imports[arg])
                             case _:
-                                warnings.warn(f"Unsupported fixup: {self.fixups[idx]}")
+                                # we should be fine ignoring these; they're for the VM.
+                                params.append(arg)
                     else:
                         params.append(arg)  # append literal integer
 
