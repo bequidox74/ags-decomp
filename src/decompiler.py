5a24b7b30cad76ea82c1703ba560a64f31a34cd1
bequidox74 <aw4yland04@proton.me>
2026-09-18 12:27:51 +0300

    implement block building

--- src/decompiler.py
+++ src/decompiler.py
@@ -8,10 +8,13 @@ from syntax_tree import STFunction, STScript
 
 
 class Decompiler:
+    type _Block = list[Instruction]
+
     @dataclass
     class _FuncState:
         func: Function
         leaders: set[Instruction] = field(default_factory=set)
+        blocks: dict[int, Decompiler._Block] = field(default_factory=dict)
 
     _BRANCH: ClassVar[set[Opcode]] = {
         Opcode.JMP,
@@ -36,6 +39,7 @@ class Decompiler:
     def _do_func(self, func: Function) -> STFunction:
         fs = self._FuncState(func)
         self._find_leaders(fs)
+        self._build_blocks(fs)
         return STFunction(func.name)
 
     def _find_leaders(self, fstate: Decompiler._FuncState) -> None:
@@ -53,6 +57,20 @@ class Decompiler:
                 leaders.add(func.lookup[target.to])
             if inst.opcode in self._TERMINATORS and i + 1 < len(func.instructions):
                 leaders.add(func.instructions[i + 1])
+
+    def _build_blocks(self, fstate: Decompiler._FuncState) -> None:
+        blocks: list[Decompiler._Block] = []
+
+        current: list[Instruction] = []
+        for inst in fstate.func.instructions:
+            if inst in fstate.leaders and current:
+                blocks.append(current)
+                current = []
+            current.append(inst)
+        if current:
+            blocks.append(current)
+
+        fstate.blocks = {b[0].offset.script: b for b in blocks}
 
     def _make_script(self, funcs: list[STFunction]) -> STScript:
         return STScript(funcs)
